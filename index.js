4c6543ed5b0fe66644f2cc59370bd057308b11b4
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-29 23:43:47 +0600

    admin access

--- index.js
+++ index.js
@@ -95,6 +95,14 @@ async function run() {
                 const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
                 res.send({ result, token });
             });
+            // admin access verify from db
+
+            app.get('/admin/:email', async (req, res) => {
+                const email = req.params.email;
+                const user = await userCollection.findOne({ email: email });
+                const isAdmin = user.role === 'admin';
+                res.send({ admin: isAdmin })
+            })
 
 
         }
