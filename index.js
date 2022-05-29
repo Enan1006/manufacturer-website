9d91e793f12235ff5c7aaee7b7cc73bd49d26358
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-29 23:42:38 +0600

    user add  route

--- index.js
+++ index.js
@@ -64,6 +64,23 @@ async function run() {
                 const users = await userCollection.find().toArray();
                 res.send(users);
             });
+            app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+                const email = req.params.email;
+                const requester = req.decoded.email;
+                const requesterAccount = await userCollection.findOne({ email: requester })
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: email };
+                    const updateDoc = {
+                        $set: { role: 'admin' },
+                    };
+                    const result = await userCollection.updateOne(filter, updateDoc);
+                    res.send(result);
+                }
+                else {
+                    res.status(403).send({ message: 'forbidden' })
+                }
+
+            });
 
         }
 
