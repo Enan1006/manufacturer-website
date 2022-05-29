95a3e9473f414670d06ad325b1b23590f4c04087
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-29 23:36:24 +0600

    function added

--- index.js
+++ index.js
@@ -13,7 +13,37 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k2t5c.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect()
+        const productCollection = client.db("eCarManufacturer").collection("manufacturer");
+        const userCollection = client.db('eCarManufacturer').collection('infos');
 
+        function verifyJWT(req, res, next) {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: 'UnAuthorized access' });
+            }
+            const token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+                if (err) {
+                    return res.status(403).send({ message: 'Forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            });
+
+        }
+
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('running eCarManufacturer Manufacturer server');
